9e765e448271b683978b641d5e88b55ab1cbbf5e
Argus Sun <argus1@users.noreply.github.com>
2024-05-22 13:30:32 -0700

    Create stability_ai_diff_req.py

---
+++ stability_ai_diff_req.py
@@ -0,0 +1,19 @@
+{
+        "text_prompts": [
+            {
+                "text": string,
+                "weight": float
+            }
+        ],
+        "height": int,
+        "width": int,
+        "cfg_scale": float,
+        "clip_guidance_preset": string,
+        "sampler": string,
+        "samples",
+        "seed": int,
+        "steps": int,
+        "style_preset": string,
+        "extras" :JSON object
+        
+}
